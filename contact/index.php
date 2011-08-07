ba6e9a9b414835b4c251b6e4a7622b3264a980da
Mike Weiss <mikeweiss@wonderwoman.dreamhost.com>
2011-08-07 11:19:01 -0700

    about and contact changed

--- contact/index.php
+++ contact/index.php
@@ -3,8 +3,7 @@ $path ='../';
 require_once('../header.php');
 ?>
 <div id="about">
-If you book shows in New Brunswick, NJ and would like to be added to the google calendar please email <a href="mailto:calendar@nbshows.org">calendar@nbshows.org</a>.
-</div>
+We need your help! If you book shows in New Brunswick, NJ and would like to be added to the google calendar please email <a href="mailto:calendar@nbshows.org">calendar@nbshows.org</a>.</div>
 <?php
 require_once('../footer.php');
 ?>     
