8e78ee63e388264a77967d5c5813862ec4f06d1b
Mike Weiss <mikeweiss@wonderwoman.dreamhost.com>
2011-08-06 23:11:12 -0700

    Became Anonymous

--- about/index.php
+++ about/index.php
@@ -5,8 +5,6 @@ require_once('../header.php');
 <div id="about">
 
 <p>NBshows gets it's show listings from a community supported <a href="https://www.google.com/calendar/embed?src=2auatm870r5a2as9ukgmnp3jq0%40group.calendar.google.com&ctz=America/New_York">google calendar</a>.  The google calendar is used to help organize and book shows. It also attempts to prevent show clashing by listing local shows in one place.</p>
-<p>Logo designed by Evan Weiss<br>
-Website by Mike Weiss  </p> 
 </div>
 <?php
 require_once('../footer.php');
--- contact/index.php
+++ contact/index.php
@@ -3,7 +3,7 @@ $path ='../';
 require_once('../header.php');
 ?>
 <div id="about">
-If you book shows in New Brunswick, NJ and would like to be added to the google calendar please email <a href="mailto:michaelcweiss@gmail.com">michaelcweiss@gmail.com</a>.
+If you book shows in New Brunswick, NJ and would like to be added to the google calendar please email <a href="mailto:calendar@nbshows.org">calendar@nbshows.org</a>.
 </div>
 <?php
 require_once('../footer.php');
