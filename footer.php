6653b23b4e7028cb56c4074dcd25a8a8c0fa8ebf
Mike Weiss <core.editor>
2011-09-15 23:10:49 -0700

    more styling stuff 	west-gate.php

--- footer.php
+++ footer.php
@@ -1,9 +1,11 @@
-<div id="footer"><a href="<?php echo $path; ?>about/">About</a> | <a href="<?php echo $path; ?>contact/">List a Show</a>
-<br>Shows are typically $5- so remember to bring some cash and support touring bands!
+<div id="footer">
 
 <div>
-<img src="images/deannadidthis.jpg">
+<img src="<?php echo $path; ?>images/deannadidthis.jpg">
 </div>
+<a href="<?php echo $path; ?>about/">About</a> | <a href="<?php echo $path; ?>contact/">List a Show</a>
+<br>Shows are typically $5- so remember to bring some cash and support touring bands!
+
 
 </div>
 </div>
