370dca17ee241337e236db96eb105c7fbc44ca9f
Michael Weiss <core.editor>
2012-06-24 17:16:02 -0700

    add $path var to fix twitter logo on other pages

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<div id="footer"><a href="<?php echo $path; ?>about/">About</a> | <a href="<?php echo $path; ?>listashow/">List a Show</a> |  <a href="http://twitter.com/diyshws" target="_blank"><img class="footer-icon" src="images/twitter-bird-16x16.png"> Blog</a>
+<div id="footer"><a href="<?php echo $path; ?>about/">About</a> | <a href="<?php echo $path; ?>listashow/">List a Show</a> |  <a href="http://twitter.com/diyshws" target="_blank"><img class="footer-icon" src="<?php echo $path ?>images/twitter-bird-16x16.png"> Blog</a>
 <br>Shows are typically $5- so remember to bring some cash and support touring bands!
 </div>
 </div>
